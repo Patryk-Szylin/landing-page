1024460bff79a0a9ef613ce76c858a61417bcdb9
Patryk Szylin <patryk.szylin.dev@gmail.com>
2019-08-07 13:22:17 +0100

    Removed unecessary files and index.html now points to correct (generated) stylesheet for mdb

--- gulpfile.js
+++ gulpfile.js
@@ -49,24 +49,6 @@ function clean() {
 
 // Bring third party dependencies from node_modules into vendor directory
 function modules() {
-  // Bootstrap
-  var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
-    .pipe(gulp.dest('./vendor/bootstrap'));
-  // // Font Awesome CSS
-  // var fontAwesomeCSS = gulp.src('./node_modules/@fortawesome/fontawesome-free/css/**/*')
-  //   .pipe(gulp.dest('./vendor/fontawesome-free/css'));
-  // // Font Awesome Webfonts
-  // var fontAwesomeWebfonts = gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
-  //   .pipe(gulp.dest('./vendor/fontawesome-free/webfonts'));
-  // // jQuery Easing
-  // var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
-  //   .pipe(gulp.dest('./vendor/jquery-easing'));
-  // // jQuery
-  // var jquery = gulp.src([
-  //   './node_modules/jquery/dist/*',
-  //   '!./node_modules/jquery/dist/core.js'
-  // ])
-  //   .pipe(gulp.dest('./vendor/jquery'));
   var mdb = gulp.src([
     "./external/mdb/js/*.js"
   ])
@@ -92,7 +74,7 @@ function modules() {
   ])
     .pipe(gulp.dest("./vendor/mdbcss/modules"));
 
-  return merge(/*bootstrap, fontAwesomeCSS, fontAwesomeWebfonts, jquery, jqueryEasing,*/ mdb, mdbaddons, mdbmodules, mdbstyles, mdbstylesaddons, mdbstylesmodules);
+  return merge(mdb, mdbaddons, mdbmodules, mdbstyles, mdbstylesaddons, mdbstylesmodules);
 }
 
 // CSS task
